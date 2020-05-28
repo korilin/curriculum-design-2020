e56bcfadcea06fd1130408b09ace8b083e60150f
aruki <zqljiebin@gmail.com>
2020-05-28 21:50:19 +0800

    重开

--- project-service/src/main/java/com/arukione/curriculum_design/CurriculumDesignApplication.java
+++ project-service/src/main/java/com/arukione/curriculum_design/CurriculumDesignApplication.java
@@ -1,4 +1,4 @@
-package com.arukione.curriculumdesign;
+package com.arukione.curriculum_design;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CurriculumDesignApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CurriculumDesignApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CurriculumDesignApplication.class, args);
+	}
 
 }
---
+++ project-service/src/test/java/com/arukione/curriculum_design/CurriculumDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arukione.curriculum_design;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurriculumDesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
